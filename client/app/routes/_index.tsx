081b7d2d4eaeeac891b516a939a35f698240d669
daltkb <daltkben@gmail.com>
2023-10-24 11:05:38 -0500

    events loader

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,4 +1,11 @@
 import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+type Event = {
+  id: string;
+  title: string;
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +14,21 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// Get events
+export const loader = async () => {
+  const apiUrl = "http://localhost:8000/api/events/";
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+  return json(data);
+};
+
 export default function Index() {
+  const events = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      {events.map((event: Event) => (
+        <p key={event.id}>{event.title}</p>
+      ))}
     </div>
   );
 }
