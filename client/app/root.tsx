0d652baeb05c8eb18ed09214e4c3bab49447c11d
daltkb <daltkben@gmail.com>
2023-10-24 14:34:56 -0500

    add shadcn/ui

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -9,8 +9,11 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import globalCss from "./styles/globals.css";
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: globalCss },
 ];
 
 export default function App() {
--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@app/components/ui/button";
 import type { MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
@@ -14,21 +15,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-// Get events
 export const loader = async () => {
   const apiUrl = "http://localhost:8000/api/events/";
   const res = await fetch(apiUrl);
+  if (res.status != 200) {
+    throw new Response("Not found", { status: 404 });
+  }
   const data = await res.json();
+
   return json(data);
 };
 
 export default function Index() {
   const events = useLoaderData<typeof loader>();
   return (
-    <div>
-      {events.map((event: Event) => (
-        <p key={event.id}>{event.title}</p>
-      ))}
-    </div>
+    <>
+      <div>
+        {events.map((event: Event) => (
+          <p key={event.id}>{event.title}</p>
+        ))}
+      </div>
+      <Button>Hello</Button>
+    </>
   );
 }
