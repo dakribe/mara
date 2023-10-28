9363031d94cf2d5ecc76b8c746bd60843e540f01
daltkb <daltkben@gmail.com>
2023-10-28 08:39:46 -0500

    remove spring

--- server/src/main/java/com/dakribe/run/event/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dakribe.run.event;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-
-@Repository
-public interface EventRepository extends CrudRepository<Event, UUID> {}
--- server/src/main/java/com/dakribe/run/event/exception/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dakribe.run.event.exception;
-
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(String message) {
-        super(message);
-    }
-}
