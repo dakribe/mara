18045a41152c194ace9f521588a375584f6850fc
daltkb <daltkben@gmail.com>
2023-11-30 04:14:15 -0600

    test ci

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,10 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
 
   return (
     <>
@@ -25,11 +25,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      <p class="read-the-docs">Hello world</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
