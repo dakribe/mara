da34a375e6d9764af34f510b14d56aad940ae310
daltkb <daltkben@gmail.com>
2023-12-20 13:30:47 -0600

    Generate user queries

--- internal/postgres/query.sql
+++ internal/postgres/query.sql
@@ -16,3 +16,16 @@ RETURNING *;
 -- name: DeleteEvent :exec
 DELETE FROM events
 WHERE id = $1;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE username = $1 LIMIT 1;
+
+-- name: CreateUser :one
+INSERT INTO users (
+  username,
+  password
+) VALUES (
+  $1, $2
+)
+RETURNING *;
