164e5b39c3f98588ecc6ca7bf77a7be1344d9be5
dakribe <dakribe94@gmail.com>
2024-07-02 22:08:41 -0500

    add routes and pages

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,35 +1,20 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Router } from "@solidjs/router";
+import { Home } from "./pages/home";
+import { SignUp } from "./pages/signup";
+import { Login } from "./pages/login";
+import { Dashboard } from "./pages/dashboard";
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
